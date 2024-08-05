3aadd4c9107582eb88f88767285d3da409795a4f
ApBin <apbin@foxmail.com>
2024-08-05 17:09:53 +0800

    引用

--- test_8_4/main.cpp
+++ test_8_4/main.cpp
@@ -97,9 +97,9 @@ void STPush(ST& rs, STDataType x)
 int& STTop(ST& rs)
 {
 	//assert(rs.top > 0);
-	return rs.a[rs.top];
+	return rs.a[rs.top - 1];
 }
-
+/*
 int main()
 {
 	// 调⽤全局的
@@ -108,7 +108,76 @@ int main()
 	STPush(st1, 1);
 	STPush(st1, 2);
 	cout << STTop(st1) << endl;
-	STTop(st1) += 10;
+	// 修改栈顶的数据呢
+	STTop(st1) += 1;
 	cout << STTop(st1) << endl;
+	return 0;
+}
+
+typedef struct SeqList
+{
+	int a[10];
+	int size;
+}SLT;
+// ⼀些主要⽤C代码实现版本数据结构教材中，使⽤C++引⽤替代指针传参，⽬的是简化程序
+//避开复杂的指针，但是很多同学没学过引⽤，导致⼀头雾⽔。
+void SeqPushBack(SLT& sl, int x)
+{
+	//...
+}
+typedef struct ListNode
+{
+	int val;
+	struct ListNode* next;
+}LTNode, *PNode;
+
+// 指针变量也可以取别名，这⾥LTNode*& phead就是给指针变量取别名
+// 这样就不需要⽤⼆级指针了，相对⽽⾔简化了程序
+//void ListPushBack(LTNode** phead, int x)
+//void ListPushBack(LTNode*& phead, int x)
+void ListPushBack(PNode& phead, int x)
+{
+	PNode newnode = (PNode)malloc(sizeof(LTNode));
+	newnode->val = x;
+	newnode->next = NULL;
+	if (phead == NULL)
+	{
+	phead = newnode;
+	}
+	else
+	{
+	//...
+	}
+}
+
+int main()
+{
+	const int a = 10;
+	// 编译报错：error C2440: “初始化”: ⽆法从“const int”转换为“int &”
+	// 这⾥的引⽤是对a访问权限的放⼤
+	//int& ra = a;
+	// 这样才可以
+	const int& ra = a;
+	// 编译报错：error C3892: “ra”: 不能给常量赋值
+	//ra++;
+	// 这⾥的引⽤是对b访问权限的缩⼩
+	int b = 20;
+	const int& rb = b;
+	// 编译报错：error C3892: “rb”: 不能给常量赋值
+	//rb++;
+	return 0;
+}
+*/
+int main()
+{
+	int a = 10;
+	const int& ra = 30;
+	// 编译报错: “初始化”: ⽆法从“int”转换为“int &”
+	// int& rb = a * 3;
+	const int& rb = a*3;
+	double d = 12.34;
+	// 编译报错：“初始化”: ⽆法从“double”转换为“int &”
+	// int& rd = d;
+	const int& rd = d;
 	return 0;
 }
